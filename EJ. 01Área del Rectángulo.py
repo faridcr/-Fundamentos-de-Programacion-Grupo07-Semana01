a5aeb68995449d6399b464ba42811eebed15c874
Josue David Mazuelos Valqui <010301josue@gmail.com>
2026-09-01 21:33:28 -0500

    Delete EJ. 01Área del Rectángulo.py

--- EJ. 01Área del Rectángulo.py
+++
@@ -1,9 +0,0 @@
-base = float(input("ingrese la base: "))
-altura = float(input("ingrese la altura: "))
-
-area = base * altura 
-perimetro = 2 * (base + altura)
-
-print("Area:", area)
-print("Perimetro:", perimetro)
-
